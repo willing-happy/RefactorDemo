be749c0157f6f34b371aec234e9a41f6af701399
Doris Zhang <willing_happy@163.com>
2018-07-18 22:12:48 +0800

    initial commit

---
+++ src/main/java/rentalstore/Rental.java
@@ -0,0 +1,19 @@
+package rentalstore;
+
+public class Rental {
+    private Movie movie;
+    private int dayRented;
+
+    public Rental(Movie movie, int dayRented) {
+        this.movie = movie;
+        this.dayRented = dayRented;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public int getDayRented() {
+        return dayRented;
+    }
+}
