0753d92fad4a17956fc3f939a784b61ce5fe472a
Doris Zhang <willing_happy@163.com>
2018-07-18 22:45:38 +0800

    add unit test

--- src/main/java/rentalstore/Customer.java
+++ src/main/java/rentalstore/Customer.java
@@ -11,7 +11,7 @@ public class Customer {
         this.name = name;
     }
 
-    public void addRental(Rental arg){
+    public void addRental(Rental arg) {
         rentals.addElement(arg);
     }
 
@@ -19,38 +19,38 @@ public class Customer {
         return name;
     }
 
-    public String statement(){
+    public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
         Enumeration rentals = this.rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
-        while(rentals.hasMoreElements()){
-            double thisAmount =0;
+        while (rentals.hasMoreElements()) {
+            double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
 
-            switch (each.getMovie().getPriceCode()){
+            switch (each.getMovie().getPriceCode()) {
                 case Movie.REGULAR:
                     thisAmount += 2;
-                    if(each.getDayRented() > 2){
-                        thisAmount+=(each.getDayRented() - 2) * 1.5;
+                    if (each.getDayRented() > 2) {
+                        thisAmount += (each.getDayRented() - 2) * 1.5;
                     }
                     break;
                 case Movie.NEW_RELEASE:
-                    thisAmount+=each.getDayRented()*3;
+                    thisAmount += each.getDayRented() * 3;
                     break;
-                    case Movie.CHILDRENS:
-                        thisAmount+=1.5;
-                        if(each.getDayRented() > 3){
-                            thisAmount += (each.getDayRented() -3)*1.5;
-                        }
-                        break;
+                case Movie.CHILDRENS:
+                    thisAmount += 1.5;
+                    if (each.getDayRented() > 3) {
+                        thisAmount += (each.getDayRented() - 3) * 1.5;
+                    }
+                    break;
             }
 
             //add frequent renter points
-            frequentRenterPoints ++;
+            frequentRenterPoints++;
             //add bonus for a two day new release rental
-            if((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDayRented() > 1){
-                frequentRenterPoints ++;
+            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDayRented() > 1) {
+                frequentRenterPoints++;
             }
 
             //show figures for this rental
@@ -59,8 +59,8 @@ public class Customer {
         }
 
         //add footer lines
-        result += "Amount owed is" + String.valueOf(totalAmount) + "\n";
-        result += "You earned" + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
     }
 }
---
+++ src/test/java/rentalstore/CustomerTest.java
@@ -0,0 +1,103 @@
+package rentalstore;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class CustomerTest {
+
+    private Customer customer = new Customer("Jerry");
+
+    @Test
+    void should_return_correct_statement_given_customer_has_no_rental() {
+        String statement = customer.statement();
+        assertEquals("Rental Record for Jerry\nAmount owed is 0.0\nYou earned 0 frequent renter points", statement);
+    }
+
+    @Test
+    void should_return_correct_statement_given_customer_has_rent_one_regular_movie_for_1_day() {
+        Movie regularMovie = new Movie("Titanic", 0);
+        Rental oneDayRental = new Rental(regularMovie, 1);
+        customer.addRental(oneDayRental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for Jerry\n" +
+                "\t" + regularMovie.getTitle() + "\t2.0\n" +
+                "Amount owed is 2.0\n" +
+                "You earned 1 frequent renter points", statement);
+    }
+
+    @Test
+    void should_return_correct_statement_given_customer_has_rent_one_regular_movie_for_3_day() {
+        Movie regularMovie = new Movie("Titanic", 0);
+        Rental threeDayRental = new Rental(regularMovie, 3);
+        customer.addRental(threeDayRental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for Jerry\n" +
+                "\t" + regularMovie.getTitle() + "\t3.5\n" +
+                "Amount owed is 3.5\n" +
+                "You earned 1 frequent renter points", statement);
+    }
+
+    @Test
+    void should_return_correct_statement_given_customer_has_rent_one_new_release_movie_for_1_day() {
+        Movie newReleaseMovie = new Movie("Titanic", 1);
+        Rental oneDayRental = new Rental(newReleaseMovie, 1);
+        customer.addRental(oneDayRental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for Jerry\n" +
+                "\t" + newReleaseMovie.getTitle() + "\t3.0\n" +
+                "Amount owed is 3.0\n" +
+                "You earned 1 frequent renter points", statement);
+    }
+
+    @Test
+    void should_return_correct_statement_given_customer_has_rent_one_new_release_movie_for_2_day() {
+        Movie newReleaseMovie = new Movie("Titanic", 1);
+        Rental twoDayRental = new Rental(newReleaseMovie, 2);
+        customer.addRental(twoDayRental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for Jerry\n" +
+                "\t" + newReleaseMovie.getTitle() + "\t6.0\n" +
+                "Amount owed is 6.0\n" +
+                "You earned 2 frequent renter points", statement);
+    }
+
+    @Test
+    void should_return_correct_statement_given_customer_has_rent_one_child_movie_for_1_day() {
+        Movie childrenMovie = new Movie("Titanic", 2);
+        Rental oneDayRental = new Rental(childrenMovie, 1);
+        customer.addRental(oneDayRental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for Jerry\n" +
+                "\t" + childrenMovie.getTitle() + "\t1.5\n" +
+                "Amount owed is 1.5\n" +
+                "You earned 1 frequent renter points", statement);
+    }
+
+    @Test
+    void should_return_correct_statement_given_customer_has_rent_one_child_movie_for_4_day() {
+        Movie childrenMovie = new Movie("Titanic", 2);
+        Rental fourDayRental = new Rental(childrenMovie, 4);
+        customer.addRental(fourDayRental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for Jerry\n" +
+                "\t" + childrenMovie.getTitle() + "\t3.0\n" +
+                "Amount owed is 3.0\n" +
+                "You earned 1 frequent renter points", statement);
+    }
+
+
+}
