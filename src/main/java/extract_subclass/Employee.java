4dc2d4c40e23b9bf2d27f7b1a49a6a55c9efd9ba
Doris Zhang <doris.zhang@oocl.com>
2018-07-25 19:59:21 +0800

    add extract subclass

---
+++ src/main/java/extract_subclass/Employee.java
@@ -0,0 +1,13 @@
+package extract_subclass;
+
+public class Employee {
+    private int rate;
+
+    public Employee(int rate) {
+        this.rate = rate;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+}
---
+++ src/main/java/extract_subclass/JobItem.java
@@ -0,0 +1,31 @@
+package extract_subclass;
+
+public class JobItem {
+    private int unitPrice;
+    private int quantity;
+    private Employee employee;
+    private boolean isLabor;
+
+    public JobItem(int unitPrice, int quantity, Employee employee, boolean isLabor) {
+        this.unitPrice = unitPrice;
+        this.quantity = quantity;
+        this.employee = employee;
+        this.isLabor = isLabor;
+    }
+
+    public int getTotalPrice(){
+        return getUnitPrice() * quantity;
+    }
+
+    private int getUnitPrice() {
+        return isLabor ? employee.getRate() : unitPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+}
