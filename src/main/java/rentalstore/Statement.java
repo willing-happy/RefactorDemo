f5e2a2adbe74a3f92e131ae68ed748e9001a79ce
Doris Zhang <willing_happy@163.com>
2018-08-05 08:40:49 +0800

    form template method

--- src/main/java/rentalstore/Statement.java
+++ src/main/java/rentalstore/Statement.java
@@ -1,4 +1,23 @@
 package rentalstore;
 
-public class Statement {
+import java.util.Enumeration;
+
+public abstract class Statement {
+    public String value(Customer customer) {
+        Enumeration rentals = customer.getRentals();
+        String result = headerString(customer);
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += eachRentalString(each);
+        }
+        result += footerString(customer);
+        return result;
+    }
+
+    abstract String headerString(Customer customer);
+
+    abstract String eachRentalString(Rental rental);
+
+    abstract String footerString(Customer customer);
+
 }
--- src/main/java/rentalstore/TextStatement.java
+++ src/main/java/rentalstore/TextStatement.java
@@ -1,21 +1,19 @@
 package rentalstore;
 
 
-import java.util.Enumeration;
 
 public class TextStatement extends Statement {
-    public String value(Customer customer) {
-        Enumeration rentals = customer.getRentals();
-        String result = "Rental Record for " + customer.getName() + "\n";
-        while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
-            //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
-        }
 
-        //add footer lines
-        result += "Amount owed is " + String.valueOf(customer.getTotalCharge()) + "\n";
-        result += "You earned " + String.valueOf(customer.getTotalFrequentRenterPoints()) + " frequent renter points";
-        return result;
+    String footerString(Customer customer) {
+        return "Amount owed is " + String.valueOf(customer.getTotalCharge()) + "\n" +
+                "You earned " + String.valueOf(customer.getTotalFrequentRenterPoints()) + " frequent renter points";
+    }
+
+    String eachRentalString(Rental each) {
+        return "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+    }
+
+    String headerString(Customer customer) {
+        return "Rental Record for " + customer.getName() + "\n";
     }
 }
