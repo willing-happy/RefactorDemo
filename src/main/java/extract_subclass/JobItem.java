c9a54da391ca132d254d0cc2fc78f565b48b34c7
Doris Zhang <doris.zhang@oocl.com>
2018-08-07 13:17:16 +0800

    Revert "extract sub class"
    This reverts commit 214eecf

--- src/main/java/extract_subclass/JobItem.java
+++ src/main/java/extract_subclass/JobItem.java
@@ -3,18 +3,25 @@ package extract_subclass;
 public class JobItem {
     private int unitPrice;
     private int quantity;
+    private Employee employee;
+    private boolean isLabor;
 
-    public JobItem(int unitPrice, int quantity) {
+    public JobItem(int unitPrice, int quantity, Employee employee, boolean isLabor) {
         this.unitPrice = unitPrice;
         this.quantity = quantity;
+        this.employee = employee;
+        this.isLabor = isLabor;
     }
 
     public int getTotalPrice(){
         return getUnitPrice() * quantity;
     }
 
-    int getUnitPrice() {
-        return unitPrice;
+    private int getUnitPrice() {
+        return isLabor ? employee.getRate() : unitPrice;
     }
 
+    public Employee getEmployee() {
+        return employee;
+    }
 }
